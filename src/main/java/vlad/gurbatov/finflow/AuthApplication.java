b0ef6bddd5f3498ad8a470695ef20bc5b0237d90
Salenhard <bratiski@mail.ru>
2026-06-18 15:46:22 +0500

    init commit

---
+++ src/main/java/vlad/gurbatov/finflow/AuthApplication.java
@@ -0,0 +1,13 @@
+package vlad.gurbatov.finflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthApplication.class, args);
+	}
+
+}
